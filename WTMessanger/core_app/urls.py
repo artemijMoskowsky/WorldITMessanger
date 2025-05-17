363fe6f521fd56ff902f911bb7e62225785c13dc
Julia Ovcharenko <samsung2100yuliyu@gmail.com>
2025-05-17 11:54:23 +0300

    Add html-code for home/my_publications pages

--- WTMessanger/core_app/urls.py
+++ WTMessanger/core_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CoreView
+from .views import CoreView, MyPublicationsView
 
 
 urlpatterns = [
     path('', CoreView.as_view(), name='core'),
+    path('my_publications/', MyPublicationsView.as_view(), name='my_publications'),
 ]
--- WTMessanger/core_app/views.py
+++ WTMessanger/core_app/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class CoreView(TemplateView):
     template_name = "core.html"
+
+class MyPublicationsView(TemplateView):
+    template_name = "my_publications.html"
